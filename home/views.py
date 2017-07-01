8b23a9069fd09ed85fbde212d91754b1a41c248e
Lucas V. Oliveira <lucas.o@live.com>
2017-07-01 01:27:33 -0300

    updated

--- home/views.py
+++ home/views.py
@@ -14,26 +14,43 @@ from collections import Counter
 
 def home_index(request):
 
+    if 'debug' in request.GET:
+        debug_url = request.GET['debug']
+
+        links, nodes_score = wikilinks.get_article_nb_links_and_scores_norm(debug_url)
+
+        return HttpResponse(json.dumps(links)+ "\n\n\n" + json.dumps(nodes_score))
+
     if 'page' in request.GET:
         try:
+
+            links, nodes_score = wikilinks.get_article_nb_links_and_scores_norm(request.GET['page'],2, 8)
+
             #links_scores = wikilinks.get_links_score(request.GET['page'], True)
             links_scores = Counter()
 
-            article, _ = wikilinks.get_or_create_article_by_url(request.GET['page'])
+            # article, _ = wikilinks.get_or_create_article_by_url(request.GET['page'])
 
-            #Populate links scores
-            for art_link in article.links.all():
-                art_link_url = art_link.link.url
-                links_scores[art_link_url] = art_link.score
-                #page_links.append([article.title, art_link_url])
+            # links_scores[article.title] = 1
 
-            #page_links = [[request.GET['page'], link_text] for link_text, _ in links_scores][:10]
-            page_links = [[article.title, link_text] for link_text, _ in links_scores.most_common(10)]
+            # #Populate links scores
+            # for art_link in article.links.all():
+
+            #     #Check if the url of the article url points to some article
+            #     if art_link.link.article: #If so, get the article title
+            #         link_title = art_link.link.article.title
+            #     else: #If not, get the url only
+            #         link_title = art_link.link.url
+            #     links_scores[link_title] = art_link.score
+            #     #page_links.append([article.title, art_link_url])
+
+            # #page_links = [[request.GET['page'], link_text] for link_text, _ in links_scores][:10]
+            # page_links = [[article.title, link_text] for link_text, _ in links_scores.most_common(15)]
 
             return render(request, "pages.html", {
                 'links_scores': links_scores,
-                'page_links': json.dumps(page_links),
-                'links_score_dict_json': json.dumps(dict(links_scores))
+                'page_links': json.dumps(links),
+                'links_score_dict_json': json.dumps(nodes_score)
             })
         except PageDoesNotExists:
             return render(request, "pages.html", {
--- home/wikilinks.py
+++ home/wikilinks.py
@@ -8,6 +8,93 @@ import re
 
 from .models import WikiArticle as WA, WikiUrl, ArticleLink
 
+def normalize_counter(counter_dict, method="higher_count"):
+    if method == "higher_count":
+        norm_value = counter_dict.most_common(1)[0][1]
+    elif method == "sum_one":
+        norm_value = sum(counter_dict.values())
+
+    norm_counter_dict = Counter()
+    for key, value in counter_dict.items():
+        norm_counter_dict[key] = value / norm_value
+
+    return norm_counter_dict
+
+def get_article_nb_links_and_scores_norm(url, max_deep=1, top_n=1):
+    """Function to normalize _get_article_nb_links_and_scores function."""
+    links_dict, score_counter = _get_article_nb_links_and_scores(url, max_deep=max_deep, top_n=top_n)
+
+    all_links = list()
+    for links in links_dict.values():
+        all_links += list(links)
+
+    norm_score_counter = normalize_counter(score_counter)
+
+    return all_links, norm_score_counter
+
+
+def _get_article_nb_links_and_scores(url, links_dict=None, score_counter=None, base_score=1, current_deep=1, max_deep=1, top_n=10):
+    """Function to accumulate article neighborhood scores recursively."""
+
+    article, _ = get_or_create_article_by_url(url)
+    
+    # if article == None:
+    #     return None, None
+
+    #Starts the objects if this is the first iteration
+    if score_counter == None:
+        score_counter = Counter()
+        score_counter[article.title] = 1
+
+    if links_dict == None:
+        links_dict = dict()
+
+
+    #If this article has not already been proceced, init its link dict
+    if article.title not in links_dict:
+        links_dict[article.title] = set()
+    else: #else, return and skip it
+        return
+
+    art_link_scores_counter = Counter()
+    #Get article links scores
+    for art_link in article.links.all():
+        art_link_scores_counter[art_link] = art_link.score
+
+    #Normalize links raw counts
+    art_link_scores_counter = normalize_counter(art_link_scores_counter)
+
+    #Iterate the top n article links
+    for art_link, art_link_score in art_link_scores_counter.most_common(top_n):
+
+        #If we haven't reached the max deep, keeps the recursion passing the new objects
+        if current_deep < max_deep:
+            art_link_url = art_link.link.url
+            _get_article_nb_links_and_scores(art_link_url, links_dict, score_counter, art_link_score, current_deep+1, max_deep, top_n)
+            #art_link_art, _ = get_or_create_article_by_url(art_link_url)
+
+        #Get proper title and append link and score
+        art_link_title = get_artlink_title(art_link)
+        links_dict[article.title].add((article.title, art_link_title))
+        score_counter[art_link_title] += art_link_score * base_score
+
+    return links_dict, score_counter
+
+
+
+def get_artlink_title(art_link):
+    #Since the art_link is loaded at the begging
+    #if any change occurrs on its derived objects, it must be reloaded
+    #The only problem is that it do not find new articles on its wikiurl
+    #It is not a major problem since the next time it loads, it fix it, so we will keep like this
+
+    #Check if the url of the article url points to some article
+    if art_link.link.article: #If so, get the article title
+        return art_link.link.article.title
+
+    return art_link.link.url
+
+
 def get_or_create_article_by_url(url):
     """Return article that the passed url-lang points to. If it does not exists, create it."""
 
@@ -38,7 +125,7 @@ def get_or_create_article_by_url(url):
         article.save()
 
         #Get links scores
-        links_scores = get_art_links_score(art, normalize=True)
+        links_scores = get_art_links_score(art, normalize=False)
 
         #Create article links
         for link, score in links_scores:
@@ -109,11 +196,13 @@ def get_art_links_score(art, normalize=False):
     #Sort and get tuple
     sorted_links_scores = links_text_counter.most_common()
 
-    if normalize:
-        #Get the higher score value and divide everything by it
-        #Normalizing by the higher score instead of sum everything to on
-        #Avoid diferent number of links per page to get higher scores than other pages
-        higher_score = sorted_links_scores[0][1]
-        sorted_links_scores = [(links_case_dict[link], score/higher_score) for link, score in sorted_links_scores]
+    # if normalize:
+    #     #Get the higher score value and divide everything by it
+    #     #Normalizing by the higher score instead of sum everything to on
+    #     #Avoid diferent number of links per page to get higher scores than other pages
+    #     higher_score = sorted_links_scores[0][1]
+    #     sorted_links_scores = [(links_case_dict[link], score/higher_score) for link, score in sorted_links_scores]
+
+    sorted_links_scores = [(links_case_dict[link], score) for link, score in sorted_links_scores]
 
     return sorted_links_scores
